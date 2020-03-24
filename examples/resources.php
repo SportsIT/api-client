754ac2df290ee0e64fa1197856fc17bda84c1dd2
Wizofgoz <wizofgoz2@gmail.com>
2020-03-24 16:37:25 -0700

    Added example for getting resources and their resource areas

---
+++ examples/resources.php
@@ -0,0 +1,26 @@
+<?php
+require '../vendor/autoload.php';
+
+$clientID = '<replace with client ID>';
+$clientSecret = '<replace with client secret>';
+$companyCode = '<replace with company code>';
+
+$dateFormat = 'Y-m-d\TH:i:s';
+
+$config = new \Dash\Configuration($clientID, $clientSecret, $companyCode);
+$client = new \Dash\Client($config);
+
+// we want all resources so no filters are needed
+$filters = [];
+
+// include the resource areas for each resource
+$includes = [
+    'resourceAreas',
+];
+
+// Call authenticate first to get an access token
+$response = $client->authenticate()
+    ->get(\Dash\Client::buildIndexRequestUri('resources', $filters, $includes));
+
+// decode the json data to associative array
+$data = json_decode($response->getBody()->getContents(), true);
